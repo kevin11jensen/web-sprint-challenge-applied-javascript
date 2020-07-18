ecdef6d9c24a9b27fa4b1b939b64662d128c4bb7
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-17 18:17:18 -0700

    starting on header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,13 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+    
+    return (
+        
+    )
+}
