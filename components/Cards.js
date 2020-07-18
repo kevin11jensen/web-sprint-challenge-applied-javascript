52714274e1a7c13d6f53302811af9df96c7ad215
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-17 18:36:20 -0700

    article cards complete

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,68 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const cards = document.querySelector('.cards-container');
+// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+    let info = response.data.articles;
+    console.log(response.data.articles);
+
+// You will be creating a component for each 'article' in the list.
+
+//articles in list: javascript, bootstrap, technology, jquery, node
+Object.values(info.bootstrap).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+Object.values(info.javascript).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+Object.values(info.jquery).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+Object.values(info.node).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+
+Object.values(info.technology).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+
+
+
+
+})
+.catch(error => {
+    console.log('Error!', error);
+})
+
+function cardCreator(headline, authorPhoto, authorName) {
+    //create elements
+    const card = document.createElement('div'),
+    cardHeadline = document.createElement('div'),
+    cardAuthorDiv = document.createElement('div'),
+    cardImgDiv = document.createElement('div'),
+    cardImg = document.createElement('img'),
+    cardAuthor = document.createElement('span');
+
+        //add classes
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthorDiv.classList.add('author');
+    cardImgDiv.classList.add('img-container');
+
+    //append
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthorDiv);
+    cardAuthorDiv.appendChild(cardImgDiv);
+    cardImgDiv.appendChild(cardImg);
+    cardAuthorDiv.appendChild(cardAuthor);
+
+    //content
+    cardHeadline.textContent = headline;
+    cardImg.src = authorPhoto;
+    cardAuthor.textContent = authorName;
+
+    return card;
+}
